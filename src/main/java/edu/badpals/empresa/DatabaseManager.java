87b4140c3ac2b0561188f98e1be1f6869f1a3adc
yelkov <yelkovq@gmail.com>
2024-11-09 19:29:56 +0100

    ejercicio 2_1 completado

--- src/main/java/edu/badpals/empresa/DatabaseManager.java
+++ src/main/java/edu/badpals/empresa/DatabaseManager.java
@@ -1,15 +1,17 @@
 package edu.badpals.empresa;
 
 import java.sql.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class DatabaseManager {
-    private final String URL = "jdbc:mysql://localhost:3306/bdempresa";
-    private final String USER = "root";
-    private final String PASSWORD = "root";
-    private Connection connection = null;
-    public Statement statement = null;
+    private final static String URL = "jdbc:mysql://localhost:3306/bdempresa";
+    private final static String USER = "root";
+    private final static String PASSWORD = "root";
+    private static Connection connection = null;
+    public static Statement statement = null;
 
-    public void conectarDB(){
+    public static void conectarDB(){
         try{
             if(connection == null){
                 connection = DriverManager.getConnection(URL, USER, PASSWORD);
@@ -22,7 +24,71 @@ public class DatabaseManager {
         }
     }
 
-    public void desconectarDB(){
+    public static void subirSalarioDepartamento(int cantidadSubida, int numDepartamento){
+        try{
+            int numFilasAfectadas = statement.executeUpdate("update empregado set salario = salario +" + cantidadSubida + " where num_departamento_pertenece ="+numDepartamento+";" );
+            if(numFilasAfectadas > 0){
+                statement.executeQuery("select nome_departamento from departamento where NUM_DEPARTAMENTO ="+numDepartamento+";");
+                ResultSet rs = statement.getResultSet();
+                while(rs.next()){
+                    String nome_departamento = rs.getString("nome_departamento");
+                    System.out.println("Se ha subido el salario de "+numFilasAfectadas+" empleados del departamento "+nome_departamento+" la cantidad de "+cantidadSubida+ "." );
+                }
+                rs.close();
+            }else{
+                System.out.println("No se ha subido el salario de ningún trabajador. Es posible que el número de departamento introducido no exista o no tenga empleados asignados.");
+            }
+
+        }catch (SQLException e){
+            System.out.println("Error al subir el salario");
+        }
+    }
+
+    public static void crearDepartamento(int numDepartamento, String nomeDepartamento, String nssDirige){
+        try{
+            ResultSet chkNumDepartamento = statement.executeQuery("select 1 from departamento where NUM_DEPARTAMENTO ="+numDepartamento+";");
+            if(chkNumDepartamento.next()){
+                    System.out.println("El número de departamento "+numDepartamento+" ya existe.");
+                    return;
+                }
+            chkNumDepartamento.close();
+
+            ResultSet chkNombreDepartamento = statement.executeQuery("select 1 from departamento where NOME_DEPARTAMENTO ='"+nomeDepartamento+"';");
+            if(chkNombreDepartamento.next()){
+                System.out.println("El nombre de departamento introducido ya existe en la base de datos");
+                return;
+            }
+            chkNombreDepartamento.close();
+
+            ResultSet chkEmpleado = statement.executeQuery("select 1 from empregado where NSS = '"+nssDirige+"'");
+            if(!chkEmpleado.next()){
+                System.out.println("El empleado introducido no existe");
+                return;
+            }
+            chkEmpleado.close();
+
+            LocalDate fechaActual = LocalDate.now();
+            statement.executeUpdate("INSERT INTO DEPARTAMENTO(NUM_DEPARTAMENTO,NOME_DEPARTAMENTO,NSS_DIRIGE,DATA_DIRECCION) VALUES ("+numDepartamento+",'"+nomeDepartamento+"','"+nssDirige+"','"+fechaActual+"') ");
+            System.out.println("El departamento " + nomeDepartamento +" se ha creado exitosamente.");
+
+        }catch (SQLException e){
+            System.out.println("Error al crear el departamento.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void borrarEmpleadoProyecto(String nss, int numProyecto){
+        try{
+            int numFilasAfectadas = statement.executeUpdate("delete from empregado_proxecto where NSS_EMPREGADO = '"+nss+"' and NUM_PROXECTO = "+numProyecto+";");
+            System.out.println("Registro de proyecto borrado. Filas afectadas: "+numFilasAfectadas);
+        }catch (SQLException e){
+            System.out.println("Error al borrar el registro de proyecto");
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void desconectarDB(){
         if(statement != null){
             try{
                 statement.close();
--- src/main/java/edu/badpals/empresa/Main.java
+++ src/main/java/edu/badpals/empresa/Main.java
@@ -1,23 +1,30 @@
 package edu.badpals.empresa;
 
+import javax.xml.crypto.Data;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        DatabaseManager dbm = new DatabaseManager();
-        dbm.conectarDB();
-        try(ResultSet rs = dbm.statement.executeQuery("select * from departamento")){
-            while(rs.next()){
-                System.out.println(rs.getInt("num_departamento"));
-                System.out.println(rs.getString("nome_departamento"));
-                System.out.println(rs.getString("nss_dirige"));
-                System.out.println(rs.getDate("data_direccion"));
-            }
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
+        DatabaseManager.conectarDB();
+
+        //ejercicio2_1();
+
+    }
+
+    public static void ejercicio2_1(){
+        DatabaseManager.subirSalarioDepartamento(27,3);
+        DatabaseManager.subirSalarioDepartamento(1000,10);
+        DatabaseManager.crearDepartamento(1,"VENTAS","1231231");
+        DatabaseManager.crearDepartamento(7,"VENTAS","1231231");
+        DatabaseManager.crearDepartamento(8,"ADMINISTRACION","9999999");
+        DatabaseManager.borrarEmpleadoProyecto("9990009",7);
+    }
+
+    public static void ejercicio2_2(){
+
     }
 }
